66648b7549275c472f295393f1e7aff80edc3f1f
Ninh Thai Lam <thailamstudy@gmail.com>
2024-08-01 18:02:33 +0700

    Refine board and create annouce

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,30 @@ function Square ({value, onSquareClick}: SquareProps) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true)
   const [squares, setSquares] = useState(Array(9).fill('') as string[])
-  
-  function handleClick(i: number) {
-    if (squares[i]) {
+
+  function calculateWinner (squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i]
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+    return null
+  }
+
+  function handleClick (i: number) {
+    if (squares[i] || calculateWinner(squares)) {
       return
     }
     const nextSquares = squares.slice()
@@ -31,8 +52,18 @@ export default function Board() {
     setSquares(nextSquares)
     setXIsNext(!xIsNext)
   }
+
+  const winner = calculateWinner (squares)
+  let status
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O")
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0] }onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1] }onSquareClick={() => handleClick(1)}/>
